95782204b8de13a2be3448f726c05e2ca824cadd
Petricioli <miguel.petricioli@gmail.com>
2021-09-29 15:20:58 -0500

    Creacion de template layaut.blade y uso del mismo

--- resources/views/add-note.blade.php
+++ resources/views/add-note.blade.php
@@ -1,33 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-    <div class="wrap">
-        <header class="head">
-            <a href="#" class="logo"></a>
-
-            <nav class="main-nav">
-                <ul class="main-nav-list">
-                    <li class="main-nav-item">
-                        <a href="/static/notes.html" class="main-nav-link">
-                            <i class="icon icon-th-list"></i>
-                            <span>Ver notas</span>
-                        </a>
-                    </li>
-                    <li class="main-nav-item active">
-                        <a href="/static/add-note.html" class="main-nav-link">
-                            <i class="icon icon-pen"></i>
-                            <span>Nueva nota</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+@extends('layout')
+@section('title', 'Crea una nota')
+@section('content')
         <main class="content">
             <div class="cards">
                 <div class="card card-center">
@@ -47,17 +20,5 @@
                 </div>
             </div>
         </main>
-        <footer class="foot">
-            <div class="ad">
-                <p>
-                    Esta aplicación es desarrollada en el curso
-                    <a href="https://styde.net/laravel-6">Primeros pasos con Laravel 6</a>.
-                </p>
-            </div>
-            <div class="license">
-                <p>© 2019 Derechos Reservados - Styde Limited</p>
-            </div>
-        </footer>
-    </div>
-</body>
-</html>
+        
+@endsection
--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -1,33 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-    <div class="wrap">
-        <header class="head">
-            <a href="#" class="logo"></a>
-
-            <nav class="main-nav">
-                <ul class="main-nav-list">
-                    <li class="main-nav-item active">
-                        <a href="/static/notes.html" class="main-nav-link">
-                            <i class="icon icon-th-list"></i>
-                            <span>Ver notas</span>
-                        </a>
-                    </li>
-                    <li class="main-nav-item">
-                        <a href="/static/add-note.html" class="main-nav-link">
-                            <i class="icon icon-pen"></i>
-                            <span>Nueva nota</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+@extends('layout')
+@section('title', 'Listado de notas')
+@section('content')
         <main class="content">
             <div class="cards">
                 
@@ -160,17 +133,5 @@
                 </div>
             </div>
         </main>
-        <footer class="foot">
-            <div class="ad">
-                <p>
-                    Esta aplicación es desarrollada en el curso
-                    <a href="https://styde.net/laravel-6">Primeros pasos con Laravel 6</a>.
-                </p>
-            </div>
-            <div class="license">
-                <p>© 2019 Derechos Reservados - Styde Limited</p>
-            </div>
-        </footer>
     </div>
-</body>
-</html>
+@endsection
