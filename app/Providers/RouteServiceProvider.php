523c1896bf3b3d078337fe16bd1c0a5ca8d560ca
Petricioli <miguel.petricioli@gmail.com>
2021-09-29 12:52:26 -0500

    Mis cambios, donde se crea acceso a archivos css

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Route::pattern('id', '[0-9]+');
         $this->configureRateLimiting();
 
         $this->routes(function () {
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,36 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Styde';
+
+    */
+
+Route::get('/', function() {
+    //return 'Aqui estará nuestro listado de notas';
+    $notes = [ 
+        'Mi primera nota',
+        'Mi segunda nota',
+        'Mi tercer nota',
+        'Mi Cuarta Nota',
+    ];
+    return view('notes', ['notes' => $notes]);
 });
+
+
+Route::get('notas/crear', function() {
+    //return 'Aqui podemos ver el formulario para crear notas';
+    return view('add-note');
+});
+
+Route::get('notas/{id}/editar', function($id){
+    return 'Aquí podemos editar la nota ??'.$id;
+});
+
+Route::get('notas/{id}', function($id)
+{
+    return 'Aqui veremos el detalle de la nota: '.$id;
+});
+
